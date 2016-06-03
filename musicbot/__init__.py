c23d13756f433a18307ddf2f57839c9984e46648
nepeat <nepeat@users.noreply.github.com>
2016-06-02 16:16:47 -0800

    Sentry exception reporting support in bot.

--- musicbot/__init__.py
+++ musicbot/__init__.py
@@ -1,8 +1,12 @@
 import discord.opus
-
+import os
+from raven import Client
 from musicbot.bot import MusicBot
 
 if not discord.opus.is_loaded():
     discord.opus.load_opus('opus')
+
+if 'SENTRY_DSN' in os.environ:
+    client = Client(dsn=os.environ["SENTRY_DSN"])
 
 __all__ = ['MusicBot']
