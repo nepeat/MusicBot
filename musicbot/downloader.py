fd787581a0456c6e655d158081e8932326dba9f3
nepeat <nepeat@users.noreply.github.com>
2016-06-20 01:04:59 -0800

    Use a ProcessPoolExecutor for fetching video information.
    A process pool should stop the stuttering in the voice channel when the GIL is taken up by youtube-dl.

--- musicbot/downloader.py
+++ musicbot/downloader.py
@@ -6,7 +6,7 @@ import redis
 import youtube_dl
 
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from musicbot.connections import redis_pool
 
 ytdl_format_options = {
@@ -28,6 +28,8 @@ ytdl_format_options = {
 # Fuck your useless bugreports message that gets two link embeds and confuses users
 youtube_dl.utils.bug_reports_message = lambda: ''
 
+thread_pool = ProcessPoolExecutor(max_workers=2)
+
 '''
     Alright, here's the problem.  To catch youtube-dl errors for their useful information, I have to
     catch the exceptions with `ignoreerrors` off.  To not break when ytdl hits a dumb video
@@ -38,24 +40,26 @@ youtube_dl.utils.bug_reports_message = lambda: ''
 
 class Downloader:
     def __init__(self, download_folder=None):
-        self.redis = redis.StrictRedis(connection_pool=redis_pool)
-        self.thread_pool = ThreadPoolExecutor(max_workers=4)
-        self.unsafe_ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-        self.safe_ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-        self.safe_ytdl.params['ignoreerrors'] = True
         self.download_folder = download_folder
-
-        if download_folder:
-            otmpl = self.unsafe_ytdl.params['outtmpl']
-            self.unsafe_ytdl.params['outtmpl'] = os.path.join(download_folder, otmpl)
-            # print("setting template to " + os.path.join(download_folder, otmpl))
-
-            otmpl = self.safe_ytdl.params['outtmpl']
-            self.safe_ytdl.params['outtmpl'] = os.path.join(download_folder, otmpl)
 
     @property
     def ytdl(self):
-        return self.safe_ytdl
+        return self.get_ytdl(safe=True)
+
+    @property
+    def redis(self):
+        return redis.StrictRedis(connection_pool=redis_pool)
+
+    def get_ytdl(self, safe=False):
+        ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+        if safe:
+            ytdl.params['ignoreerrors'] = True
+
+        if self.download_folder:
+            otmpl = ytdl.params['outtmpl']
+            ytdl.params['outtmpl'] = os.path.join(self.download_folder, otmpl)
+
+        return ytdl
 
     def set_cache(self, url, data, **kwargs):
         cachekey = "musicbot:cache:" + url
@@ -87,6 +91,10 @@ class Downloader:
         except json.JSONDecodeError:
             return None
 
+    def _extract_info(self, *args, safe=False, **kwargs):
+        ytdl = self.get_ytdl(safe)
+        return ytdl.extract_info(*args, **kwargs)
+
     async def extract_info(self, loop, *args, on_error=None, retry_on_error=False, **kwargs):
         """
             Runs ytdl.extract_info within the threadpool. Returns a future that will fire when it's done.
@@ -94,14 +102,14 @@ class Downloader:
             on_error as an argument.
         """
 
-        info = await loop.run_in_executor(self.thread_pool, functools.partial(self.get_cache, args[0], **kwargs))
+        info = await loop.run_in_executor(thread_pool, functools.partial(self.get_cache, args[0], **kwargs))
         if info:
             return info
 
         if callable(on_error):
             try:
-                info = await loop.run_in_executor(self.thread_pool, functools.partial(self.unsafe_ytdl.extract_info, *args, **kwargs))
-                loop.run_in_executor(self.thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
+                info = await loop.run_in_executor(thread_pool, functools.partial(self._extract_info, *args, **kwargs))
+                loop.run_in_executor(thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
                 return info
             except Exception as e:
 
@@ -119,15 +127,15 @@ class Downloader:
                 if retry_on_error:
                     return await self.safe_extract_info(loop, *args, **kwargs)
         else:
-            info = await loop.run_in_executor(self.thread_pool, functools.partial(self.unsafe_ytdl.extract_info, *args, **kwargs))
-            loop.run_in_executor(self.thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
+            info = await loop.run_in_executor(thread_pool, functools.partial(self._extract_info, *args, **kwargs))
+            loop.run_in_executor(thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
             return info
 
     async def safe_extract_info(self, loop, *args, **kwargs):
-        info = await loop.run_in_executor(self.thread_pool, functools.partial(self.get_cache, args[0], **kwargs))
+        info = await loop.run_in_executor(thread_pool, functools.partial(self.get_cache, args[0], **kwargs))
         if info:
             return info
 
-        info = await loop.run_in_executor(self.thread_pool, functools.partial(self.safe_ytdl.extract_info, *args, **kwargs))
-        loop.run_in_executor(self.thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
+        info = await loop.run_in_executor(thread_pool, functools.partial(self._extract_info, safe=True, *args, **kwargs))
+        loop.run_in_executor(thread_pool, functools.partial(self.set_cache, args[0], info, **kwargs))
         return info
